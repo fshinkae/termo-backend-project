b823fffd20395aff6e943dbd4e0efe1116035866
fshinkae_meli <felipe.shinkae@mercadolivre.com>
2025-10-21 19:28:02 -0400

    fix: fix imports

--- src/routes/gameRoutes.js
+++ src/routes/gameRoutes.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import { finishGame } from '../controllers/endgameController.js';
 import { validateGameFinish } from '../middlewares/validation.js';
-import { fakeJWT } from '../utils/auth.js';
+import { authenticateToken } from '../middlewares/auth.js';
+import Keyword from '../models/Keyword.js';
+import History from '../models/History.js';
 
 const router = express.Router();
 
